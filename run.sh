c2463f263f9c34ba1201cce0b5e5771d94aab74c
calint <calin.tenitchi@gmail.com>
2023-08-21 14:27:37 +0200

    .

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -e
 
 gcc -fdiagnostics-color=always -fno-exceptions -Wfatal-errors -pedantic -pedantic-errors \
     -Wall -Wextra -Wconversion -Wcast-align -Wcast-qual -Wshadow -Wsign-conversion \
-    -Wno-unused-parameter -Wno-unused-result\
+    -Wno-unused-parameter -Wno-unused-result \
     -Os -o roome roome.c
 ./roome
